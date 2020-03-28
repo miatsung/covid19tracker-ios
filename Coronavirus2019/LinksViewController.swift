3ae1d56c2cc192eb0944fde5a884d9e90f516745
Mia Tsung <mia.tsung311@gmail.com>
2020-03-28 15:04:13 -0700

    embedded in tab view controller

---
+++ Coronavirus2019/LinksViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LinksViewController.swift
+//  Coronavirus2019
+//
+//  Created by Mia Tsung on 3/28/20.
+//  Copyright © 2020 Mia Tsung. All rights reserved.
+//
+
+import UIKit
+
+class LinksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
